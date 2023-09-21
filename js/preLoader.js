b7426305e4b41c8019494599b43388378fca1880
Mahdi <mahdipakravan1998@gmail.com>
2023-09-21 18:28:04 +0330

    Preloader added

---
+++ js/preLoader.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    // Hide the loading screen and show the content when everything is fully loaded
+    document.querySelector('.loading-screen').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+};
